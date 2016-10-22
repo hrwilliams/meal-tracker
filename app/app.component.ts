32bcdedcb610c827559b075f7c555cf47ec696e2
Halle Williams <williams.hr@gmail.com>
2016-10-22 16:52:45 -0700

    show and hide edit form

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,19 +5,25 @@ import { Component } from '@angular/core';
   template: `
   <div class ="container">
     <h1>Meal Tracker</h1>
-    <h3 (click)="showDetails(currentFood)" *ngFor="let currentFood of foods">{{ currentFood.name + " ||" }} {{ currentFood.calories + " calories || "}} {{"details: " + currentFood.details }} </h3>
-    <h1>Edit Food</h1>
-    <div>
-      <label>Enter Food Name:</label>
-      <input [(ngModel)]="selectedFood.name">
+    <div *ngFor="let currentFood of foods">
+      <h3>{{ currentFood.name + " ||" }} {{ currentFood.calories + " calories || "}} {{"details: " + currentFood.details }} </h3>
+    <button (click)="showDetails(currentFood)">Edit</button>
     </div>
-    <div>
-      <label>Enter calories:</label>
-      <input [(ngModel)]="selectedFood.calories">
-    </div>
-    <div>
-      <label>Enter details:</label>
-      <input [(ngModel)]="selectedFood.details">
+    <div *ngIf="selectedFood">
+      <h1>Edit Food</h1>
+      <div>
+        <label>Enter Food Name:</label>
+        <input [(ngModel)]="selectedFood.name">
+      </div>
+      <div>
+        <label>Enter calories:</label>
+        <input [(ngModel)]="selectedFood.calories">
+      </div>
+      <div>
+        <label>Enter details:</label>
+        <input [(ngModel)]="selectedFood.details">
+        <button (click)="finishedEditing()">Done</button>
+      </div>
     </div>
   </div>
   `
@@ -28,11 +34,13 @@ export class AppComponent {
     new Food ("Test Food", 100, "delete me on the backend later"),
     new Food ("Test Food2", 200, "delete me on the backend later")
   ];
-  selectedFood: Food = this.foods[0];
+  selectedFood: Food = null;
   showDetails(clickedFood: Food) {
     this.selectedFood = clickedFood;
   }
-
+  finishedEditing() {
+    this.selectedFood = null;
+  }
 }
 
 export class Food {
