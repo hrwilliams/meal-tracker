49a747032697ab4106c969a54b1e22f45799b233
Halle Williams <williams.hr@gmail.com>
2016-10-22 14:00:25 -0700

    add functionality to edit food: creating method to select food,  add edit form

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,20 @@ import { Component } from '@angular/core';
   template: `
   <div class ="container">
     <h1>Meal Tracker</h1>
-    <h3 *ngFor="let currentFood of foods">{{ currentFood.name + " ||" }} {{ currentFood.calories + " calories || "}} {{"details: " + currentFood.details }} </h3>
+    <h3 (click)="showDetails(currentFood)" *ngFor="let currentFood of foods">{{ currentFood.name + " ||" }} {{ currentFood.calories + " calories || "}} {{"details: " + currentFood.details }} </h3>
+    <h1>Edit Food</h1>
+    <div>
+      <label>Enter Food Name:</label>
+      <input [(ngModel)]="selectedFood.name">
+    </div>
+    <div>
+      <label>Enter calories:</label>
+      <input [(ngModel)]="selectedFood.calories">
+    </div>
+    <div>
+      <label>Enter details:</label>
+      <input [(ngModel)]="selectedFood.details">
+    </div>
   </div>
   `
 })
@@ -15,6 +28,11 @@ export class AppComponent {
     new Food ("Test Food", 100, "delete me on the backend later"),
     new Food ("Test Food2", 200, "delete me on the backend later")
   ];
+  selectedFood: Food = this.foods[0];
+  showDetails(clickedFood: Food) {
+    this.selectedFood = clickedFood;
+  }
+
 }
 
 export class Food {
