d9629c39604a6835f5f6a7897b157d921b6a9396
Halle Williams <williams.hr@gmail.com>
2016-10-22 13:36:27 -0700

    create food class/constructor, page displays food name

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>Meal Tracker</h1>
+  <div class ="container">
+    <h1>Meal Tracker</h1>
+    <h3 *ngFor="let currentFood of foods">{{ currentFood.name }}</h3>
+  </div>
   `
 })
 
 export class AppComponent {
+  public foods: Food[] = [
+    new Food ("Test Food", 100, "delete me on the backend later"),
+    new Food ("Test Food2", 200, "delete me on the backend later")
+  ];
+}
 
+export class Food {
+  constructor(public name: string, public calories: number, public details: string){  
+  }
 }
